a3d066be34f5a8e26ace373570fa72e0ffbe621e
Paolo Bernasconi <paolo.enrico.bernasconi@gmail.com>
2023-02-04 17:43:15 -0800

    add

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,6 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
+        <Image src={"/bench.svg"} alt="bench" width={100} height={100} />
         <div className={styles.center}>
           <p>
             <strong>Broken Bench</strong> is more than just a supper club, it's
