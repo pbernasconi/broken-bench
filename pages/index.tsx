5d677173280b22332222ad90d5bb33f2947316e2
Paolo Bernasconi <paolo.enrico.bernasconi@gmail.com>
2023-02-06 19:41:49 -0600

    fix date

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,7 +36,7 @@ export default function Home() {
         </div>
         <div className={styles.center}>
           <p className={styles.nextDate}>
-            Upcoming <strong>Saturday March 18</strong>
+            Upcoming <strong>Saturday March 4</strong>
           </p>
         </div>
       </main>
